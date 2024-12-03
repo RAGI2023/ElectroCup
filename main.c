35eea6583fca360c5cd3c44edf9518f706177940
RAGI <yinzhe23@bupt.edu.cn>
2024-12-03 22:35:05 +0800

    全是问题

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
-#include <math.h>
 #include <msp430.h>
 #include <stdint.h>
-#include "intrinsics.h"
 #include "lcd/lcd.h"
 #include "button/button.h"
 #include "Lissajous/Lissajous.h"
@@ -9,99 +7,28 @@
 #include "res/res.h"
 #include "ctrl/ctrl.h"
 #include "adc/adc.h"
+#include "timer/timer.h"
 #define PI 3.14159265358979323846
-#define  SAMPLE_CNT 50
-#define  CAL_NO 1
-#define BAR_NUM 4 //偶数！
 
-uint16_t adc_buffer_[SAMPLE_CNT];
-uint16_t adc_buffer[SAMPLE_CNT];
-// const uchar font5x8[5] = {
-//     0x7E, // 01111110 -> 第一列
-//     0x09, // 00001001 -> 第二列
-//     0x09, // 00001001 -> 第三列
-//     0x09, // 00001001 -> 第四列
-//     0x7E  // 01111110 -> 第五列
-// };
+uint16_t adc_buffer[100] = {0};
 
-
+extern uint16_t time_stamp;
+extern unsigned long int val, val2;
 
 void main(void)
 {
 //    SMCLK 50kHz
     WDTCTL = WDTPW | WDTHOLD;       // stop watchdog timer
+//    timer_init();
+    ADC_init_intref_repeating(adc_buffer, 100);
 
-    // 测试时钟频率
-    // P1DIR |= BIT0 + BIT4; //设置P1.0 1.4为输出
-    // P1SEL |= BIT0 + BIT4; //设置P1.0输出ACLK，P1.4输出SMCLK
-
-    P1DIR |= BIT1;                  // Set P1.0 to output direction
-    //第一个采样点真没法用
-    ADC_init_intref_repeating(adc_buffer_, SAMPLE_CNT);
-    __delay_cycles(1000);
-    
-    LissajousInit();
     initial_lcd();
 
-    const uint8_t analyse_start = 2; //反三角第一个计算点
-    int i;
-    
-    for (i = 0; i < SAMPLE_CNT; i++){
-        adc_buffer[i] = adc_buffer_[i]; //复制过来
-        lcd_deltaphi(0, 0, adc_buffer[i]/1023.0 * 2.5);
-        lcd_deltaphi(1, 0, i);
-    }
-    uint16_t amp1 = adc_buffer[1], amp2 = adc_buffer[1];
-    
-    
-    for(i = 1; i < SAMPLE_CNT; i++){
-        if(adc_buffer[i] > amp1){
-            amp1 = adc_buffer[i];
-        }
-        if (adc_buffer[i] < amp2){
-            amp2 = adc_buffer[i];
-        }
-        
-    }
-    float amp = (amp1 - amp2)/1023.0 * 2.5 /2;
-    float theta_for_mean[BAR_NUM];
-    float theta_bar = 0;
-    for(i = 0; i < BAR_NUM; i++){
-        theta_for_mean[i] = (adc_buffer[i+analyse_start] / 1023.0 * 2.5 - 1.0) / 1.0;
-        if (theta_for_mean[i] > 1){
-            theta_for_mean[i] = 1;
-        }else if (theta_for_mean[i] < -1){
-            theta_for_mean[i] = -1;
-        }
-        lcd_deltaphi(0, 0, theta_for_mean[i]);
-        lcd_deltaphi(1, 0, i);
-        theta_for_mean[i] = asinf(theta_for_mean[i]);
-        if (theta_for_mean[i] < 0){
-            theta_for_mean[i] += PI;
-        }
-    }
-    for(i = 0; i < BAR_NUM / 2; i++){
-        theta_bar += theta_for_mean[i+BAR_NUM/2] - theta_for_mean[i];
-    }
-    theta_bar /= BAR_NUM / 2.0 / PI * 180;
+    // button_init();
+//    P1OUT |= BIT0;
+    // lcd_deltaphi(1, 0, 1);
 
-    
-    float theta = asinf((adc_buffer[analyse_start] / 1023.0 * 2.5 - 1.0) / 1.0) / PI * 180;
-    theta -= theta_bar * analyse_start; //消除采样点之前的相位延迟
-    lcd_deltaphi(2, 0, amp1/ 1023.0 * 2.5);
-    // __delay_cycles(100000);
-    lcd_deltaphi(3, 0, amp2/ 1023.0 * 2.5);
-    // __delay_cycles(100000);
-    lcd_deltaphi(4, 0, theta);
-    // uint16_t adcdata = getADC();
-    lcd_deltaphi(5, 0, theta_bar * 100);
-    lcd_deltaphi(6, 0, adc_buffer[analyse_start] / 1023.0 * 2.5);
-   
-   
-//
-//    res_init();
-//    ctrl_init();
-
+    // P1OUT ^= BIT0;
     // button_init();
 //    set_res(100);
 //    set_freq(2);
@@ -114,26 +41,14 @@ void main(void)
 //        P1OUT |= BIT1;
 //
 //    }
-
-    
-    
-    // delay(50);
     // clear_screen();
     // DisplayLissajous(1, 1, 1, 3,PI/2);
-    // display_graphic_1X8(4, 107, &data);
-    // if (DisplayLissajous(1, 2, 0))
-    // {
-    //  P1OUT = 0x01;
-    // }else{
-    //  P1OUT ^= 0x01;
-    // delay(5000);
-    // P1OUT |= BIT0;
-    // }
-
     // set_res(1000);
-    // Transmit();
 
 
-    while(1);
+
+    while(1){
+        lcd_deltaphi(0, 0, adc_buffer[50] /1023.0 * 2.5);
+    }
 
 }
--- timer/timer.c
+++ timer/timer.c
@@ -1,10 +1,103 @@
-#include "msp430.h"
+#include "msp430g2553.h"
+#include "stdint.h"
+// SMCLK = 1MHz
 
-// MCLK = 1MHz
+uint16_t capt1 = 0;
+
+
+#define uint unsigned int
+
+#define uchar unsigned char
+
+
+uint PerVal;
+uint PerVal2;
+unsigned long int val;
+unsigned long int val2;
+
+// 时间戳 65535max
+uint16_t time_stamp = 0;
 
 void timer_init(void)
 {
-    TACCTL0 = CCIE; // Enable interrupt 0计时器
-    
+    //Timer0_A3 初始化
 
+////    P3SEL |= BIT0;
+////    P3REN |= BIT0;
+////    P3OUT &= ~BIT0;
+////    P3SEL2 &= ~BIT0;
+//
+////    TA0CCTL0 = CAP + CM_1 + CCIE + SCS + CCIS_0; // TA0CCR0 Capture mode; CCIxA;
+////    TA0CCTL1 = CAP + CM_3 + CCIE + SCS + CCIS_0;
+//    TA0CCTL2 = CAP + CM_1 + CCIE + SCS + CCIS_0;
+////    // Rising edge; interrupt enable
+//    TA0CTL |= TASSEL_2 + MC_2 + TACLR + TAIE + ID_0; // SMCLK, Cont Mode; start timer
+
+
+
+//    P1DIR&=~BIT1;  //捕捉P1.1口
+//    P1REN|=BIT1;
+//    P1OUT&=~BIT1;
+//    P1SEL |= BIT1;
+    TA0CCTL0 |= CAP + CM_1 + CCIS_0 + SCS + CCIE + TAIE;//捕获模式，上升和下降都捕获，选择CCI0A，同步，捕获中断开
+    TA0CTL |= TASSEL_2 + MC_2 + TACLR; //SMCLK=1M,连续计数模式
+
+//    P1DIR&=~BIT2; //捕捉P1.2口
+//    P1REN|=BIT2;
+//    P1OUT&=~BIT2;
+//    P1SEL |= BIT2;
+//    TA0CCTL1 |= CAP + CM_3 + CCIS_0 + SCS + CCIE;//捕获模式，上升和下降都捕获，选择CCI1A，同步，捕获中断开
+//    TA0CTL |= TASSEL_2 + MC_2 + ID_0 + TACLR; //SMCLK=1M,连续计数模式
+//    _EINT();//中断起始位
+
+    P1DIR |= BIT0;
+
+    // Timer1_A3计时器初始化 时间戳
+    TA1CCR0 = 2 - 1;                //voir texte   1/1mhz * 50000  0.5s
+    TA1CCTL0 = CCIE; // enable compare interrupt
+    TA1CTL |= (TASSEL_2 | ID_0 | MC_1 + TACLR);   //source SMCLK, pas de predivision ID_0
+//    TA1CTL |= TAIE;                 //autorisation interruption TAIE
+    __enable_interrupt();
+
+
+
+    // Timer
+
+}
+
+// Timer A1 CCR0 interrupt service routine
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void Timer_A(void)
+{
+    time_stamp++;
+//    P1OUT ^= BIT1;
+    TA1CTL&=~(TAIFG); //Reset the interrupt flag
+}
+
+//#pragma vector=TIMER0_A0_VECTOR
+//__interrupt void freq_capture(void)
+//{
+//    capt1 = TA0CCR2;
+//    P1OUT |= BIT0;
+//}
+
+
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void TIMER0_A0_ISR(void)
+
+{
+    P1OUT |= BIT0;
+  switch(TA0IV)
+  {
+    case 2:
+      val = TA0CCR1 - PerVal;
+      PerVal = TA0CCR1;
+
+      break;
+    case 4:
+      val2 = TA0CCR1 - PerVal2;
+      PerVal2 = TA0CCR1;
+      break;
+  }
 }
