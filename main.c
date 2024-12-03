22493c38e054c437472d898a4f091a275a273372
RAGI <yinzhe23@bupt.edu.cn>
2024-12-03 18:11:43 +0800

    ADC硬测方案

--- main.c
+++ main.c
@@ -10,11 +10,12 @@
 #include "ctrl/ctrl.h"
 #include "adc/adc.h"
 #define PI 3.14159265358979323846
-#define  SAMPLE_CNT 100
+#define  SAMPLE_CNT 50
 #define  CAL_NO 1
-#define BAR_NUM 10
+#define BAR_NUM 4 //偶数！
 
-uint16_t adc_buffer[SAMPLE_CNT] = {0};
+uint16_t adc_buffer_[SAMPLE_CNT];
+uint16_t adc_buffer[SAMPLE_CNT];
 // const uchar font5x8[5] = {
 //     0x7E, // 01111110 -> 第一列
 //     0x09, // 00001001 -> 第二列
@@ -29,13 +30,30 @@ void main(void)
 {
 //    SMCLK 50kHz
     WDTCTL = WDTPW | WDTHOLD;       // stop watchdog timer
+
+    // 测试时钟频率
+    // P1DIR |= BIT0 + BIT4; //设置P1.0 1.4为输出
+    // P1SEL |= BIT0 + BIT4; //设置P1.0输出ACLK，P1.4输出SMCLK
+
     P1DIR |= BIT1;                  // Set P1.0 to output direction
     //第一个采样点真没法用
-    ADC_init_intref_repeating(adc_buffer, SAMPLE_CNT);
-    uint16_t amp1 = adc_buffer[1], amp2 = adc_buffer[1];
+    ADC_init_intref_repeating(adc_buffer_, SAMPLE_CNT);
+    __delay_cycles(1000);
+    
     LissajousInit();
     initial_lcd();
+
+    const uint8_t analyse_start = 2; //反三角第一个计算点
     int i;
+    
+    for (i = 0; i < SAMPLE_CNT; i++){
+        adc_buffer[i] = adc_buffer_[i]; //复制过来
+        lcd_deltaphi(0, 0, adc_buffer[i]/1023.0 * 2.5);
+        lcd_deltaphi(1, 0, i);
+    }
+    uint16_t amp1 = adc_buffer[1], amp2 = adc_buffer[1];
+    
+    
     for(i = 1; i < SAMPLE_CNT; i++){
         if(adc_buffer[i] > amp1){
             amp1 = adc_buffer[i];
@@ -43,25 +61,41 @@ void main(void)
         if (adc_buffer[i] < amp2){
             amp2 = adc_buffer[i];
         }
-        lcd_deltaphi(0, 0, adc_buffer[i]/1023.0 * 2.5);
-        lcd_deltaphi(1, 0, i);
-        // P1OUT ^= BIT1;
+        
     }
     float amp = (amp1 - amp2)/1023.0 * 2.5 /2;
-    float theta_bar = 0.0;
-    for(i = 1; i <= BAR_NUM; i++){
-        theta_bar += asinf(adc_buffer[i] / 1023.0 * 2.5 / 1.0);
+    float theta_for_mean[BAR_NUM];
+    float theta_bar = 0;
+    for(i = 0; i < BAR_NUM; i++){
+        theta_for_mean[i] = (adc_buffer[i+analyse_start] / 1023.0 * 2.5 - 1.0) / 1.0;
+        if (theta_for_mean[i] > 1){
+            theta_for_mean[i] = 1;
+        }else if (theta_for_mean[i] < -1){
+            theta_for_mean[i] = -1;
+        }
+        lcd_deltaphi(0, 0, theta_for_mean[i]);
+        lcd_deltaphi(1, 0, i);
+        theta_for_mean[i] = asinf(theta_for_mean[i]);
+        if (theta_for_mean[i] < 0){
+            theta_for_mean[i] += PI;
+        }
     }
-    theta_bar /= BAR_NUM;
+    for(i = 0; i < BAR_NUM / 2; i++){
+        theta_bar += theta_for_mean[i+BAR_NUM/2] - theta_for_mean[i];
+    }
+    theta_bar /= BAR_NUM / 2.0 / PI * 180;
 
-    float theta = asinf(adc_buffer[1] / 1023.0 * 2.5 / 1.0);
-    theta -= theta_bar; //消除第2个采样点带来的相位延迟
+    
+    float theta = asinf((adc_buffer[analyse_start] / 1023.0 * 2.5 - 1.0) / 1.0) / PI * 180;
+    theta -= theta_bar * analyse_start; //消除采样点之前的相位延迟
     lcd_deltaphi(2, 0, amp1/ 1023.0 * 2.5);
     // __delay_cycles(100000);
     lcd_deltaphi(3, 0, amp2/ 1023.0 * 2.5);
     // __delay_cycles(100000);
-    lcd_deltaphi(4, 0, theta / PI * 180);
+    lcd_deltaphi(4, 0, theta);
     // uint16_t adcdata = getADC();
+    lcd_deltaphi(5, 0, theta_bar * 100);
+    lcd_deltaphi(6, 0, adc_buffer[analyse_start] / 1023.0 * 2.5);
    
    
 //
---
+++ timer/timer.c
@@ -0,0 +1,10 @@
+#include "msp430.h"
+
+// MCLK = 1MHz
+
+void timer_init(void)
+{
+    TACCTL0 = CCIE; // Enable interrupt 0¼ÆÊ±Æ÷
+    
+
+}
